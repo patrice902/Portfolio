9392db9542ce3b7983f15542efd7597f73a077a0
patrice902 <patricereddick902@yahoo.com>
2024-11-28 08:28:39 -0500

    Add Lightfoot into projects

--- lib/data.ts
+++ lib/data.ts
@@ -2,6 +2,7 @@ import React from "react";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import carImage from "@/public/Car.png";
+import lightFootImage from "@/public/Lightfoot.png";
 import graphicsEditorImage from "@/public/graphics-editor.png";
 import chessImage from "@/public/chess.gif";
 
@@ -133,7 +134,16 @@ export const projectsData = [
     title: "Online Chess Tournament",
     description:
       "An Online Chess Tournament Web app that schedules chess tournaments for registered users, manages Match-making, and run online chess game having video chat.",
-    tags: ["React", "JavaScript", "Material UI", "Redux", "HTML", "CSS", "Zoom", "Chess"],
+    tags: [
+      "React",
+      "JavaScript",
+      "Material UI",
+      "Redux",
+      "HTML",
+      "CSS",
+      "Zoom",
+      "Chess",
+    ],
     imageUrl: chessImage,
     projectUrl: "https://github.com/patrice902/OnlineChess",
   },
@@ -141,15 +151,55 @@ export const projectsData = [
     title: "Graphics Editor",
     description:
       "You can put images, graphics and texts into the board, and can resize/rotate/clip/edit them. Also, multi-users can join this board and draw together having fun!",
-    tags: ["React", "Redux", "Node", "Konva", "Socket.io",  "JavaScript", "HTML", "CSS"],
+    tags: [
+      "React",
+      "Redux",
+      "Node",
+      "Konva",
+      "Socket.io",
+      "JavaScript",
+      "HTML",
+      "CSS",
+    ],
     imageUrl: graphicsEditorImage,
     projectUrl: "https://github.com/patrice902/Graphics-Editor",
   },
   {
     title: "Car Painter",
     description: `You can create a scheme with a specific car model, and paint it with various logos, shapes and texts. And, you can export it and use it in games. Also, it supports multi-user/real-time collaboration for team work and fun!`,
-    tags: ["React", "Redux", "Node", "Material UI", "Express", "MySQL", "Knex", "Objection", "Konva",  "TypeScript", "JavaScript", "HTML", "CSS"],
+    tags: [
+      "React",
+      "Redux",
+      "Node",
+      "Material UI",
+      "Express",
+      "MySQL",
+      "Knex",
+      "Objection",
+      "Konva",
+      "TypeScript",
+      "JavaScript",
+      "HTML",
+      "CSS",
+    ],
     imageUrl: carImage,
+    // projectUrl: "https://github.com/patrice902/Car-Painter",
+  },
+  {
+    title: "Lightfoot",
+    description: `Lightfoot iOS/Android app helps drivers use less fuel, reduce harmful emissions and stay safe every time they drive.`,
+    tags: [
+      "React Native",
+      "Expo",
+      "Mobx",
+      "React Native Paper",
+      "TypeScript",
+      "JavaScript",
+      "Jest",
+      "HTML",
+      "CSS",
+    ],
+    imageUrl: lightFootImage,
     // projectUrl: "https://github.com/patrice902/Car-Painter",
   },
 ];
